a422b70b13da6dd0a7e4c16277fe9e8635a78aa3
Arul-Ananth <a.arul.ananth.2006@gmail.com>
2026-02-27 07:29:29 +0530

    daily tasks 27-feb-2026

---
+++ 26_02_2026/Kadenes.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class Solution {
+    public int maxSubArray(int[] nums) {
+        int maxSum = nums[0];
+        int currentSum = 0 ;
+        for ( int num : nums)
+        {
+            currentSum+=num;
+            maxSum = Math.max(maxSum , currentSum);
+            if ( currentSum < 0 ) currentSum = 0 ;
+        }
+        return maxSum;
+
+    }
+
+
+}
